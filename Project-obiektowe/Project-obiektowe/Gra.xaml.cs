1a1640f24c785697fb57a073cbfb5d499b39ad8f
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-02-09 00:53:36 +0100

    gra - zasady zwycięstwa w kolumnach i wierszach(gotowe)

--- Project-obiektowe/Project-obiektowe/Gra.xaml.cs
+++ Project-obiektowe/Project-obiektowe/Gra.xaml.cs
@@ -25,44 +25,17 @@ namespace Project_obiektowe
 			InitializeComponent();
 		}
 
-		bool a = false;
-		bool b = false;
-		private void win()
-		{
-			
 
-			if (a == true && b == true)
-			{
-				MessageBox.Show("Koniec Gry");
-			}
-		}
 
-		 public static kolo()
-		{
-			Image img = new Image();
-			img.Source = new BitmapImage(new Uri("images/1.jpng"));
-		}
+
+		bool tura = true;
 		
 		
 
 		
 	
 
-		private void Button_Click_2(object sender, RoutedEventArgs e)
-		{
-			a = true;
-			win();
-			Button b = (Button)sender;
-			b.Content = kolo();
 
-		}
-
-		private void Button_Click_3(object sender, RoutedEventArgs e)
-		{
-			b = true;
-			win();
-
-		}
 		// button - nowa gra
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
@@ -71,5 +44,250 @@ namespace Project_obiektowe
 			this.Close();
 			gra.Show();
 		}
+
+		bool end = false;
+		
+
+		// win conditions
+		public void Win()
+		{
+			// win condition (3 marks in vertical)
+
+			// 1's column
+			if (a1.Content == a2.Content && a1.Content == a3.Content && a1.IsEnabled == false && a2.IsEnabled == false && a3.IsEnabled == false)
+				end = true;
+			if (a3.Content == a4.Content && a3.Content == a5.Content && a4.IsEnabled == false && a5.IsEnabled == false && a3.IsEnabled == false)
+				end = true;
+
+			if (a2.Content == a3.Content && a3.Content == a4.Content && a2.IsEnabled == false && a3.IsEnabled == false && a4.IsEnabled == false)
+				end = true;
+
+
+			// 2's column
+			
+			if (b1.Content == b2.Content && b1.Content == b3.Content && b1.IsEnabled == false && b2.IsEnabled == false && b3.IsEnabled == false)
+				end = true;
+			if (b3.Content == b4.Content && b3.Content == b5.Content && b4.IsEnabled == false && b5.IsEnabled == false && b3.IsEnabled == false)
+				end = true;
+
+			if (b2.Content == b3.Content && b3.Content == b4.Content && b2.IsEnabled == false && b3.IsEnabled == false && b4.IsEnabled == false)
+				end = true;
+
+
+
+			// 3's column
+			if (c1.Content == c2.Content && c1.Content == c3.Content && c1.IsEnabled == false && c2.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+			if (c3.Content == c4.Content && c3.Content == c5.Content && c4.IsEnabled == false && c5.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+
+			if (c2.Content == c3.Content && c3.Content == c4.Content && c2.IsEnabled == false && c3.IsEnabled == false && c4.IsEnabled == false)
+				end = true;
+
+
+			// 4' column
+			if (d1.Content == d2.Content && d1.Content == d3.Content && d1.IsEnabled == false && d2.IsEnabled == false && d3.IsEnabled == false)
+				end = true;
+			if (d3.Content == d4.Content && d3.Content == d5.Content && d4.IsEnabled == false && d5.IsEnabled == false && d3.IsEnabled == false)
+				end = true;
+
+			if (d2.Content == d3.Content && d3.Content == d4.Content && d2.IsEnabled == false && d3.IsEnabled == false && d4.IsEnabled == false)
+				end = true;
+
+
+
+			// 5's column
+			if (e1.Content == e2.Content && e1.Content == e3.Content && e1.IsEnabled == false && e2.IsEnabled == false && e3.IsEnabled == false)
+				end = true;
+			if (e3.Content == e4.Content && e3.Content == e5.Content && e4.IsEnabled == false && e5.IsEnabled == false && e3.IsEnabled == false)
+				end = true;
+
+			if (e2.Content == e3.Content && e3.Content == e4.Content && e2.IsEnabled == false && e3.IsEnabled == false && e4.IsEnabled == false)
+				end = true;
+
+
+			// 6's column
+
+			if (f1.Content == f2.Content && f1.Content == f3.Content && f1.IsEnabled == false && f2.IsEnabled == false && f3.IsEnabled == false)
+				end = true;
+			if (f3.Content == f4.Content && f3.Content == f5.Content && f4.IsEnabled == false && f5.IsEnabled == false && f3.IsEnabled == false)
+				end = true;
+
+			if (f2.Content == f3.Content && f3.Content == f4.Content && f2.IsEnabled == false && f3.IsEnabled == false && f4.IsEnabled == false)
+				end = true;
+
+
+
+			// 7's column
+
+			if (g1.Content == g2.Content && g1.Content == g3.Content && g1.IsEnabled == false && g2.IsEnabled == false && g3.IsEnabled == false)
+				end = true;
+			if (g3.Content == g4.Content && g3.Content == g5.Content && g4.IsEnabled == false && g5.IsEnabled == false && g3.IsEnabled == false)
+				end = true;
+
+			if (g2.Content == g3.Content && g3.Content == g4.Content && g2.IsEnabled == false && g3.IsEnabled == false && g4.IsEnabled == false)
+				end = true;
+
+
+
+
+
+
+
+
+			//win condition(3 marks in level)
+			// 1 poem
+			if (a1.Content == b1.Content && a1.Content == c1.Content && a1.IsEnabled == false && c1.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+			if (b1.Content == c1.Content && b1.Content == d1.Content && b1.IsEnabled == false && c1.IsEnabled == false && d1.IsEnabled == false)
+				end = true;
+
+			if (c1.Content == d1.Content && c1.Content == e1.Content && c1.IsEnabled == false && d1.IsEnabled == false && e1.IsEnabled == false)
+				end = true;
+			if (d1.Content == e1.Content && d1.Content == f1.Content && d1.IsEnabled == false && e1.IsEnabled == false && f1.IsEnabled == false)
+				end = true;
+			if (e1.Content == f1.Content && e1.Content == g1.Content && e1.IsEnabled == false && f1.IsEnabled == false && g1.IsEnabled == false)
+				end = true;
+
+			// 2 poem
+
+			if (a2.Content == b2.Content && a2.Content == c2.Content && a2.IsEnabled == false && c2.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+			if (b2.Content == c2.Content && b2.Content == d2.Content && b2.IsEnabled == false && c2.IsEnabled == false && d2.IsEnabled == false)
+				end = true;
+
+			if (c2.Content == d2.Content && c2.Content == e2.Content && c2.IsEnabled == false && d2.IsEnabled == false && e2.IsEnabled == false)
+				end = true;
+			if (d2.Content == e2.Content && d2.Content == f2.Content && d2.IsEnabled == false && e2.IsEnabled == false && f2.IsEnabled == false)
+				end = true;
+			if (e2.Content == f2.Content && e2.Content == g2.Content && e2.IsEnabled == false && f2.IsEnabled == false && g2.IsEnabled == false)
+				end = true;
+
+// 3 poem
+
+			if (a3.Content == b3.Content && a3.Content == c3.Content && a3.IsEnabled == false && c3.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+			if (b3.Content == c3.Content && b3.Content == d3.Content && b3.IsEnabled == false && c3.IsEnabled == false && d3.IsEnabled == false)
+				end = true;
+
+			if (c3.Content == d3.Content && c3.Content == e3.Content && c3.IsEnabled == false && d3.IsEnabled == false && e3.IsEnabled == false)
+				end = true;
+			if (d3.Content == e3.Content && d3.Content == f3.Content && d3.IsEnabled == false && e3.IsEnabled == false && f3.IsEnabled == false)
+				end = true;
+			if (e3.Content == f3.Content && e3.Content == g3.Content && e3.IsEnabled == false && f3.IsEnabled == false && g3.IsEnabled == false)
+				end = true;
+
+
+			// 4 poem
+			if (a4.Content == b4.Content && a4.Content == c4.Content && a4.IsEnabled == false && c4.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+			if (b4.Content == c4.Content && b4.Content == d4.Content && b4.IsEnabled == false && c4.IsEnabled == false && d4.IsEnabled == false)
+				end = true;
+
+			if (c4.Content == d4.Content && c4.Content == e4.Content && c4.IsEnabled == false && d4.IsEnabled == false && e4.IsEnabled == false)
+				end = true;
+			if (d4.Content == e4.Content && d4.Content == f4.Content && d4.IsEnabled == false && e4.IsEnabled == false && f4.IsEnabled == false)
+				end = true;
+			if (e4.Content == f4.Content && e4.Content == g4.Content && e4.IsEnabled == false && f4.IsEnabled == false && g4.IsEnabled == false)
+				end = true;
+
+			// 5 poem
+			if (a5.Content == b5.Content && a5.Content == c5.Content && a5.IsEnabled == false && c5.IsEnabled == false && c3.IsEnabled == false)
+				end = true;
+			if (b5.Content == c5.Content && b5.Content == d5.Content && b5.IsEnabled == false && c5.IsEnabled == false && d5.IsEnabled == false)
+				end = true;
+
+			if (c5.Content == d5.Content && c5.Content == e5.Content && c5.IsEnabled == false && d5.IsEnabled == false && e5.IsEnabled == false)
+				end = true;
+			if (d5.Content == e5.Content && d5.Content == f5.Content && d5.IsEnabled == false && e5.IsEnabled == false && f5.IsEnabled == false)
+				end = true;
+			if (e5.Content == f5.Content && e5.Content == g5.Content && e5.IsEnabled == false && f5.IsEnabled == false && g5.IsEnabled == false)
+				end = true;
+
+
+
+
+
+
+
+
+
+
+
+
+			if (end == true)
+				MessageBox.Show("tralalla");
+
+		}
+
+
+
+
+
+
+
+		void DisableButton()
+		{
+			a1.IsEnabled = false;
+			a2.IsEnabled = false;
+			a3.IsEnabled = false;
+			a4.IsEnabled = false;
+			a5.IsEnabled = false;
+			b1.IsEnabled = false;
+			b2.IsEnabled = false;
+			b3.IsEnabled = false;
+			b4.IsEnabled = false;
+			b5.IsEnabled = false;
+			c1.IsEnabled = false;
+			c2.IsEnabled = false;
+			c3.IsEnabled = false;
+			c4.IsEnabled = false;
+			c5.IsEnabled = false;
+			d1.IsEnabled = false;
+			d2.IsEnabled = false;
+			d3.IsEnabled = false;
+			d4.IsEnabled = false;
+			d5.IsEnabled = false;
+			e1.IsEnabled = false;
+			e2.IsEnabled = false;
+			e3.IsEnabled = false;
+			e4.IsEnabled = false;
+			e5.IsEnabled = false;
+			f1.IsEnabled = false;
+			f2.IsEnabled = false;
+			f3.IsEnabled = false;
+			f4.IsEnabled = false;
+			f5.IsEnabled = false;
+			g1.IsEnabled = false;
+			g2.IsEnabled = false;
+			g3.IsEnabled = false;
+			g4.IsEnabled = false;
+			g5.IsEnabled = false;
+		}
+
+		// button - pola gry (30 przyciskow)
+		private void a(object sender, RoutedEventArgs e)
+		{
+			Button kolo = (Button)sender;
+			if (tura == true)
+			{
+				kolo.Content = "X";
+				tura = false;
+				
+			}
+			else if (tura == false)
+			{
+				kolo.Content = "O";
+				tura = true;
+				
+			}
+
+			kolo.IsEnabled = false;
+			Win();
+
+		}
+
+		
 	}
-}
+} 
+
